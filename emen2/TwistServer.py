64e4fd3800cb8f2f37618c7fb01b45ae71f19500
Steve Ludtke <sludtke@bcm.edu>
2004-08-25 06:16:37 +0000

    Minor.

--- emen2/TwistServer.py
+++ emen2/TwistServer.py
@@ -3,17 +3,20 @@
 # TwistSupport contains the actual XMLRPC methods
 # TwistSupport_html contains the HTML methods
 
+DBPATH="/home/stevel/emen2test"
+HTMLPATH="/home/stevel/pro/emen2/tweb"
+
 from twisted.internet import reactor
 from twisted.web import static, server
 from emen2 import TwistSupport
 from emen2 import TwistSupport_html
 
 # Change this to a directory for the actual database files
-TwistSupport.startup("/home/stevel/emen2test")
+TwistSupport.startup(DBPATH)
 TwistSupport_html.db=TwistSupport.db
 
 # Change this to point to static HTML content
-root = static.File("/home/stevel/pro/emen2/tweb")
+root = static.File(HTMLPATH)
 
 root.putChild("db",TwistSupport_html.DBResource())
 root.putChild("RPC2",TwistSupport.DBXMLRPCResource())
