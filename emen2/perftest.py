d62b33bfb898e582c93162ece13efff394ab48fc
Steve Ludtke <sludtke@bcm.edu>
2004-11-03 01:52:44 +0000

    trivial

--- emen2/perftest.py
+++ emen2/perftest.py
@@ -3,7 +3,8 @@ import time
 from xmlrpclib import *
 
 #s=Server("http://arq2:8080/RPC2")
-s=Server("http://localhost:8080/RPC2")
+s=Server("http://ncmidb2:8080/RPC2")
+#s=Server("http://localhost:8080/RPC2")
 q=s.login("root","foobar")
 
 t0=time.time()
@@ -13,6 +14,14 @@ for i in range(10000):
 t1=time.time()
 
 print "Ping: ",t1-t0, (t1-t0)/10000, 10000/(t1-t0)
+
+t0=time.time()
+for i in range(100):
+	rn=[int(random()*250000) for i in range(100)]
+	try: a=s.getrecords(rn,q,0)
+	except: pass
+t1=time.time()
+print "100/query rnd:", t1-t0, (t1-t0)/10000, 10000/(t1-t0)
 
 t0=time.time()
 for i in range(1000):
@@ -42,13 +51,13 @@ t1=time.time()
 print "10/query seq:", t1-t0, (t1-t0)/10000, 10000/(t1-t0)
 
 t0=time.time()
-for i in range(100):
+for i in range(1000):
 	rn=range(i*100,i*100+100)
 	try: a=s.getrecords(rn,q,0)
 	except: pass
 t1=time.time()
 
-print "100/query seq:", t1-t0, (t1-t0)/10000, 10000/(t1-t0)
+print "100/query seq:", t1-t0, (t1-t0)/100000, 100000/(t1-t0)
 
 t0=time.time()
 for i in range(10000):
