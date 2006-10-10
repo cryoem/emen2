458ea9ed7984814c772284e0ed4572aecfb669f9
Ian Rees <ian.rees@bcm.edu>
2006-10-10 09:54:27 +0000

    *** empty log message ***

--- emen2/web/html.py
+++ emen2/web/html.py
@@ -465,7 +465,7 @@ def html_record_dicttable(dict,proto,viewdef,missing=0,ctxid=None):
 				try: value1 = dict[match.group("var1")]
 				except:	value1 = "<span style=\"color:grey\">%s</span>"%match.group("var2")
 				if value1:
-					value = pcomments.sub("<br />",value1)
+					value = pcomments.sub("<br />",str(value1))
 				# include popup
 #				print "%s: %s"%(match.group("var1"),value)
 				popup = "onmouseover=\"tooltip_show('tooltip_%s');\" onmouseout=\"tooltip_hide('tooltip_%s');\""%(match.group("var1"),match.group("var1"))
