e8f2f4ea5c26d54538a2e6bbcf1340d8a6c89163
emen2 <emen2>
2006-11-07 18:36:56 +0000

    Trivial

--- emen2/startup.sh
+++ emen2/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python /home/emen2/EMEN2/emen2/TwistServer.py >/tmp/emen2log 2>/tmp/emen2errlog &
+python /home/emen2/emen2/TwistServer.py >/tmp/emen2log 2>/tmp/emen2errlog &
 echo $! >/tmp/emen2job
 disown %1
