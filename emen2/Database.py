2891958f8f068c392f8eab17e770e7cae0e2e33e
kwel <kwel>
2007-02-01 18:12:49 +0000

    Uncommented debug prints

--- emen2/Database.py
+++ emen2/Database.py
@@ -49,17 +49,20 @@ def DB_cleanup() :
 	"""This does at_exit cleanup. It would be nice if this were always called, but if python is killed
 	with a signal, it isn't. This tries to nicely close everything in the database so no recovery is
 	necessary at the next restart"""
-	print "Closing %d BDB databases"%(len(BTree.alltrees)+len(IntBTree.alltrees)+len(FieldBTree.alltrees))
-#	print >>sys.stderr, len(BTree.alltrees), 'BTrees'
+	print >>sys.stderr, "Closing %d BDB databases"%(len(BTree.alltrees)+len(IntBTree.alltrees)+len(FieldBTree.alltrees))
+	print >>sys.stderr, len(BTree.alltrees), 'BTrees'
 	for i in BTree.alltrees:
 		i.close()
-#	print >>sys.stderr, '\n', len(IntBTree.alltrees), 'IntBTrees'
+		sys.stderr.write('.')
+	print >>sys.stderr, '\n', len(IntBTree.alltrees), 'IntBTrees'
 	for i in IntBTree.alltrees:
 		i.close()
-#	print >>sys.stderr, '\n', len(FieldBTree.alltrees), 'FieldBTrees'
+		sys.stderr.write('.')
+	print >>sys.stderr, '\n', len(FieldBTree.alltrees), 'FieldBTrees'
 	for i in FieldBTree.alltrees:
 		i.close()
-
+		sys.stderr.write('.')
+	sys.stderr.write('\n')
 # This rmakes sure the database gets closed properly at exit
 atexit.register(DB_cleanup)
 
@@ -191,18 +194,18 @@ class BTree:
 
 	def close(self):
 		if not self.bdb: return
-#		print >>sys.stderr, '\nbegin'; sys.stderr.flush()
+		print >>sys.stderr, '\nbegin'; sys.stderr.flush()
 		try:
 			self.pcdb.close()
-#			print >>sys.stderr, '/pc'; sys.stderr.flush()
+			print >>sys.stderr, '/pc'; sys.stderr.flush()
 			self.cpdb.close()
-#			print >>sys.stderr, '/cp'; sys.stderr.flush()
+			print >>sys.stderr, '/cp'; sys.stderr.flush()
 			self.reldb.close()
-#			print >>sys.stderr, '/rel'; sys.stderr.flush()
+			print >>sys.stderr, '/rel'; sys.stderr.flush()
 		except: pass
-#		print >>sys.stderr, 'main'; sys.stderr.flush()
+		print >>sys.stderr, 'main'; sys.stderr.flush()
 		self.bdb.close()
-#		print >>sys.stderr, '/main'; sys.stderr.flush()
+		print >>sys.stderr, '/main'; sys.stderr.flush()
 		self.bdb=None
 	
 	def sync(self):
