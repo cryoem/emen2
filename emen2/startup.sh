46e3132335b38bb50ac4be264faadaa22259c513
Ian Rees <ian.rees@bcm.edu>
2007-07-24 08:54:52 +0000

    *** empty log message ***

--- emen2/startup.sh
+++ emen2/startup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-python /home/emen2/emen2/TwistServer.py >/tmp/emen2log 2>/tmp/emen2errlog &
-echo $! >/tmp/emen2job
+EMEN2LOG=`python -c "from emen2config import *;print EMEN2LOG"`
+EMEN2ERRLOG=`python -c "from emen2config import *;print EMEN2ERRLOG"`
+EMEN2JOBFILE=`python -c "from emen2config import *;print EMEN2JOBFILE"`
+EMEN2ROOT=`python -c "from emen2config import *;print EMEN2ROOT"`
+PYTHONBIN=`python -c "from emen2config import *;print PYTHONBIN"`
+
+
+python2.4 $EMEN2ROOT/TwistServer.py > $EMEN2LOG 2> $EMEN2ERRLOG &
+echo $! > $EMEN2JOBFILE
 disown %1
