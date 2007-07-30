5f20954a28212692875ca4998ca1996eb3d3b3a0
kwel <kwel>
2007-07-30 16:11:27 +0000

    *** empty log message ***

--- emen2/startup.sh
+++ emen2/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-EMEN2LOG=`python -c "from emen2config import *;print EMEN2LOG"`
-EMEN2ERRLOG=`python -c "from emen2config import *;print EMEN2ERRLOG"`
-EMEN2JOBFILE=`python -c "from emen2config import *;print EMEN2JOBFILE"`
-EMEN2ROOT=`python -c "from emen2config import *;print EMEN2ROOT"`
-PYTHONBIN=`python -c "from emen2config import *;print PYTHONBIN"`
+EMEN2LOG=`python -c "from emen2.emen2config import *;print EMEN2LOG"`
+EMEN2ERRLOG=`python -c "from emen2.emen2config import *;print EMEN2ERRLOG"`
+EMEN2JOBFILE=`python -c "from emen2.emen2config import *;print EMEN2JOBFILE"`
+EMEN2ROOT=`python -c "from emen2.emen2config import *;print EMEN2ROOT"`
+PYTHONBIN=`python -c "from emen2.emen2config import *;print PYTHONBIN"`
 
 
 python2.4 $EMEN2ROOT/TwistServer.py >> $EMEN2LOG 2>> $EMEN2ERRLOG &
