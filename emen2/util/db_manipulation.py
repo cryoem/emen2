38ffca0909e61da3d3f2c6fe5fd3996b05eecb02
edwlan <edwlan>
2008-03-28 19:55:01 +0000

    Code Refactoring

--- emen2/util/db_manipulation.py
+++ emen2/util/db_manipulation.py
@@ -1,66 +1,81 @@
 from functools import partial
 import emen2.Database
-import g
 
-#from g import debug
+import emen2.debug as _d
+d = _d.DebugState()
 
-def get_path_id(path, cur_dir=None, dbinfo=None):
-    '''raises StopIteration if path does not exist'''
-    #dbinfo=dbinfo or {}
+def get_create(recdef, param, value, db, ctxid, host=None):
+    record =  db.getindexbyvalue(param, value, ctxid, host)
+    record = db.groupbyrecorddef(record, ctxid, host).get(recdef, set())
+    if len(record) == 1:
+        record = db.getrecord(record.pop(), ctxid)
+    elif len(record) > 1:
+        raise IntegrityError('Value Not Unique')
+    else:
+        record = db.newrecord(recdef, ctxid)
+        record[param] = value
+    return record
+
+class IntegrityError(ValueError): pass
+
+class DBTree(object):
+    root = property(lambda self: self.__root)
     
-    if cur_dir == None: # if cur_dir not supplied, assume an absolute path
-        cur_dir = get_root_id(**dbinfo)
+    def __init__(self, db, ctxid, host=None, root=None):
+        self.__db = db
+        self.__ctxid = ctxid
+        self.__host = host
+        self.__root = root or min(db.getindexbyrecorddef('folder', ctxid=ctxid, host=host))
+
+    def get_path_id(self, path, cur_dir=None, dbinfo=None):
+        '''raises StopIteration if path does not exist'''
+        dbinfo=dbinfo or {}
+        cur_dir = cur_dir or self.root
+            
+        if path != []: # recurse if we are not at the end of the path list
+            tmp = self.get_child_id(path[0], cur_dir=cur_dir, **dbinfo)
+            return self.get_path_id(path[1:], tmp, dbinfo)
         
-    if path != []: # recurse if we are not at the end of the path list
-        tmp = get_child_id(path[0], cur_dir=cur_dir, **dbinfo)
-        return get_path_id(path[1:], tmp, dbinfo)
-    else: # if we are at the end of the path list, we must be where we want to be
-        return cur_dir
+        else: # if we are at the end of the path list, we must be where we want to be
+            return cur_dir
     
-def get_child_id(name, db, ctxid, cur_dir, host=None):
-    '''returns first child with a given folder_name'''
-    getrecord = partial(db.getrecord, ctxid=ctxid, host=host) # make getrecord calls shorter
-    children = db.getchildren(cur_dir, keytype='record', ctxid=ctxid, host=host)
-    g.debug('children: %s\nsubfolders: %s' % (children, db.groupbyrecorddef(children, ctxid)))
-    subfolders = unfold_dict(db.groupbyrecorddef(children, ctxid))
-    return ((rectype, rec) for (rectype, rec) in subfolders # get all children of the current record 
-                if  (getrecord(rec)['%s_name' % rectype] == name) or (str(rec) == name) # who have the right folder name or record id
-              ).next()[1]  # only return the first match
+    def get_child_id(self, name, cur_dir):
+        '''returns first child with a given folder_name'''
+        ctxid = self.__ctxid
+        host = self.__host
+        children = self.__db.getchildren(cur_dir, keytype='record', ctxid=ctxid, host=host)
+        subfolders = self.__unfold_dict(self.__db.groupbyrecorddef(children, ctxid))
+        return self.__dostuff(name, subfolders).next()[1]
+    
+    def __dostuff(self, name, subfolders):
+        getrecord = partial(self.__db.getrecord, ctxid=self.__ctxid, host=self.__host) # make getrecord calls shorter
+        for (rectype, rec) in subfolders:
+            if (str(rec) == name) or (getrecord(rec)['%s_name' % rectype] == name):
+                yield (rectype, rec)
         
+    def __unfold_dict(self, dict):
+        for key in dict:
+            for item in dict[key]:
+                yield (key, item)
 
-
-def get_root_id(db, ctxid, host=None):
-    '''gets the folder with the lowest id
-    NOTE: a better implementation might take a record and recurse upward to a record without a parent'''
-    return min(db.getindexbyrecorddef('folder', ctxid=ctxid, host=host))
-
+def get_child_id(name, db, ctxid, cur_dir=None, host=None):
+    return DBTree(db, ctxid, host).get_child_id(name, cur_dir=cur_dir)
 
 def get_child(name, db, ctxid, cur_dir, host=None):
     '''returns first child with a given folder_name'''
     return db.getrecord(get_child_id(name, db, ctxid, cur_dir), ctxid)
 
-def get_root(db, ctxid, host=None):
-    return db.getrecord(get_root_id(db, ctxid), ctxid)
+def get_path_id(path, cur_dir=None, dbinfo=None):
+    db, ctxid, host = dbinfo['db'], dbinfo['ctxid'], dbinfo['host']
+    return DBTree(db, ctxid, host).get_path_id(path, cur_dir=cur_dir)
 
 def get_path(path, cur_dir=None, dbinfo=None):
     '''raises StopIteration if path does not exist'''
-    db, ctxid = dbinfo['db'], dbinfo['ctxid']
-    g.debug('path == %s' % path)
-    return db.getrecord(get_path_id(path, cur_dir, dbinfo), ctxid)
-
-#def find_in_set(recset, dbinfo=None):
-#        dbinfo=dbinfo or {}
+    db, ctxid, host = dbinfo['db'], dbinfo['ctxid'], dbinfo['host']
+    return db.getrecord(get_path_id(path, cur_dir, dbinfo), ctxid, host=host)
         
-def unfold_dict(dict):
-    '''
-    returns a generator which yields (key,item) pairs:
-    
-    >>> a = unfold_dict(dict(a=[1,2,3], b=[2], c=[3], d=[123123,123123,23445]))
-    >>> a.next()
-    ('a', 1)
-    '''
-    for key in dict:
-        for item in dict[key]:
-            yield (key, item)
-    
-    #[ [  (yield  (key, item)) for item in dict[key] ] for key in dict ]
+def get_root_id(db, ctxid, host=None):
+    return DBTree(db, ctxid, host).root
+
+def get_root(db, ctxid, host=None):
+    return db.getrecord(get_root_id(db, ctxid, host))
