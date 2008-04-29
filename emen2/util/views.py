22ec195429da5bdc6e5fd195beb76cf306c6124e
edwlan <edwlan>
2008-04-29 17:29:37 +0000

    fixing a bug

--- emen2/util/views.py
+++ emen2/util/views.py
@@ -99,7 +99,7 @@ class View(object):
 class Page(object):
 	def __init__(self, template, db, ctxid, value_dict=None, host=None, **kwargs):
 		self.__template = template
-		self.__valuedict = {'menus': build_menus(g.PAGE_MENUS, db=db, ctxid=ctxid, host=host)}
+		self.__valuedict = {} #{'menus': build_menus(g.PAGE_MENUS, db=db, ctxid=ctxid, host=host)}
 		self.__valuedict.update(adj_dict(value_dict or {}, kwargs))
 		
 	def __unicode__(self):
