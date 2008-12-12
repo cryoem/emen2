078650a31f90f6f043b458b270d6ebddc2c7d6f3
Ian Rees <ian.rees@bcm.edu>
2008-12-12 16:01:48 +0000

    permissions fixes

--- emen2/util/core_macros.py
+++ emen2/util/core_macros.py
@@ -203,7 +203,7 @@ def getvalue(db, recset, attribute, join_func=def_join_func, **rinfo):
 	tmp = []
 	for i in recset:
 		try:
-			tmp.append(db.getrecord(i,**rinfo))
+			tmp.append(db.getrecord(i,**rinfo)
 		except:
 			print "error with macro get record: %s"%i
 	return join_func([rec[attribute] for rec in tmp if rec.has_key(attribute)])
