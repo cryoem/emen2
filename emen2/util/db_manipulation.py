4f9d702c382aba6e5602ddb7116f1a83a8d636ea
edwlan <edwlan>
2009-03-19 21:28:54 +0000

    *** empty log message ***

--- emen2/util/db_manipulation.py
+++ emen2/util/db_manipulation.py
@@ -130,8 +130,8 @@ class DBTree(object):
 	def to_path(self, recid):
 		return str.join('/', self.__to_path(recid))
 	
-	def reverse(self, name, *args, **kwargs):
-		return '/db'+(URLRegistry.reverselookup(name, *args, **kwargs) or '')
+	def reverse(self, _name, *args, **kwargs):
+		return '/db'+(URLRegistry.reverselookup(_name, *args, **kwargs) or '')
 	
 	def render_template_view(self, name, *args, **kwargs):
 		return URLRegistry.call_view(name, db=self.__db, *args, **kwargs )#ctxid=self.__ctxid, host=self.__host, 
