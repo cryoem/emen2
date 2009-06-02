19ab4898b328bdf6b28a0b9b962fb5e057e3d75e
edwlan <edwlan>
2009-06-02 18:29:32 +0000

    *** empty log message ***

--- emen2/startup.sh
+++ emen2/startup.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-EMEN2LOG=`python -c "from emen2config import *;print EMEN2LOG"`
-EMEN2ERRLOG=`python -c "from emen2config import *;print EMEN2ERRLOG"`
-EMEN2JOBFILE=`python -c "from emen2config import *;print EMEN2JOBFILE"`
-EMEN2ROOT=`python -c "from emen2config import *;print EMEN2ROOT"`
-PYTHONBIN=`python -c "from emen2config import *;print PYTHONBIN"`
+PYTHONPATH=.. python -c "from emen2config import *;print EMEN2LOG"
+PYTHONPATH=.. python -c "from emen2config import *;print EMEN2ERRLOG"
+PYTHONPATH=.. python -c "from emen2config import *;print EMEN2JOBFILE"
+PYTHONPATH=.. EMEN2ROOT=`python -c "from emen2config import *;print EMEN2ROOT"`
+echo $EMEN2ROOT
+PYTHONPATH=.. python -c "from emen2config import *;print PYTHONBIN"
 
-
-python $EMEN2ROOT/TwistServer.py >> $EMEN2LOG 2>> $EMEN2ERRLOG &
-echo $! > $EMEN2JOBFILE
-disown %1
+PYTHONPATH=.. python $EMEN2ROOT/emen2control.py --start
