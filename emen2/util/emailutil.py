d9d55fe2b09ee2c5680a63b329e8f9069a3c2516
edwlan <edwlan>
2009-08-06 16:23:53 +0000

    ''

---
+++ emen2/util/emailutil.py
@@ -0,0 +1,83 @@
+#standard emen2 imports
+from emen2.emen2config import *
+import smtplib
+from email.MIMEText import MIMEText
+#from email.mime.text import MIMEText
+
+import emen2.globalns
+g = emen2.globalns.GlobalNamespace('')
+
+def sendmailtemplate(recipient,template,ctxt=None):
+	if ctxt==None: ctxt={}
+	ctxt["recipient"]=recipient
+	ctxt["MAILADMIN"]=g.MAILADMIN
+
+
+	msg = g.templates.render_template(template, ctxt)
+	sendmailraw(recipient,msg)
+
+
+
+
+def sendmail(recipient,subject,msgtxt, ctx=None, txn=None, db=None):
+
+	msg=MIMEText(msgtxt)
+	msg['Subject'] = subject
+	msg['From'] = g.MAILADMIN
+	msg['To'] = recipient
+
+	#return
+	# Send the message via our own SMTP server, but don't include the
+	# envelope header.
+
+	s = smtplib.SMTP(g.MAILHOST)
+	s.set_debuglevel(1)
+	s.sendmail(g.MAILADMIN, [recipient], msg.as_string())
+
+
+
+
+def sendmailraw(recipient,content):
+	#return
+	s = smtplib.SMTP(g.MAILHOST)
+	s.set_debuglevel(1)
+	s.sendmail(g.MAILADMIN, [recipient], content)
+
+
+
+# Re-implement more of these features
+
+# def sendmsg(mailfrom, mailto, subject, msg, debug=0):
+#     '''All arguments are strings except debug. mailto can be several
+#     addresses, separated by spaces or commas.'''
+#     m = email.message_from_string(msg)
+#     moff, hoff = math.modf(-time.timezone / 3600.0)
+# #moff, hoff = math.modf(int(5.5*3600) / 3600.0)
+#     hoff = int(hoff)
+#     moff = int(moff * 60)
+#     t = time.localtime()
+#     hoff += (t[-1] != 0)
+#
+#     m['Date'] = time.strftime("%a, %d %b %Y %H:%M:%S ", t) + \
+#                 "%+03d%02d" % (hoff, moff)
+#     m['From'] = mailfrom
+#     m['To'] = ', '.join(re.split('[ ,]+', mailto))
+#     m['Subject'] = subject
+# #    m.add_header('User-Agent', 'Python 2.4.1 (#2, Aug 29 2005, 20:13:14)')
+#     m.add_header('User-Agent',
+#                  'Python %s [%s %s]' % \
+#                   (sys.version[ :sys.version.find('\n')].rstrip(), \
+#                    sys.platform, sys.arch))
+#     m.add_header('MIME-Version', '1.0')
+#     m.add_header('Content-Type', 'text/plain', charset='us-ascii')
+#     m.add_header('Content-Transfer-Encoding', '7bit')
+#     if debug > 1:
+#         print m.items(), '\n------------\n', m.as_string(1)
+#     else:
+#         server = smtplib.SMTP('smtp.bcm.tmc.edu')
+#         server.set_debuglevel(debug)
+#         ret = server.sendmail(mailfrom, re.split('[ ,]+', mailto),
+#                               m.as_string(1))
+#         server.quit()
+
+
--- emen2/util/utils.py
+++ emen2/util/utils.py
@@ -5,11 +5,12 @@ except ImportError: pass
 
 class prop(property):
     '''apply prop.init to a function that returns a dictionary with keys
-    fget, fset, and/or fdel in order to create a property''' 
+    fget, fset, and/or fdel in order to create a property'''
     @classmethod
     def init(cls, func):
         result = cls(**func())
         return result
+
 
 
 class BaseDecorator(object):
@@ -27,7 +28,6 @@ class BaseDecorator(object):
         self.after_hook()
         return result
 
-
 class EscapedFun(BaseDecorator):
     def __call__(self, *args, **kwargs):
         return cgi.escape(self.func(*args, **kwargs))
@@ -37,35 +37,6 @@ class ReturnWithMimeType(BaseDecorator):
     def __call__(self, *args, **kwargs):
         return self.func(*args, **kwargs), self.mime_type
 
-def return_jpg(name, bases, dict):
-    cls = type(name, bases, dict)
-    cls.mime_type = 'image/jpg'
-    return cls
-
-class ReturnAsJPG(ReturnWithMimeType):
-    __metaclass__ = return_jpg
-
-def return_png(name, bases, dict):
-    cls = type(name, bases, dict)
-    cls.mime_type = 'image/png'
-    return cls
-
-class ReturnAsPNG(ReturnWithMimeType):
-    __metaclass__ = return_png
-
-def return_html(name, bases, dict):
-    cls = type(name, bases, dict)
-    cls.mime_type = "text/html"
-    return cls
-
-class ReturnAsHTML(ReturnWithMimeType):
-    __metaclass__ = return_html
-
-class PreformattedOutp(ReturnAsHTML):
-    def __call__(self, *args, **kwargs):
-        result = ReturnAsHTML.__call__(self, *args, **kwargs)
-        return '<pre>' + result[0].encode('utf-8') + '</pre>', result[1]
-
 class MultiDecorate(BaseDecorator):
     def __init__(self, decs=[]):
         self.decorators = decs[:]
@@ -74,11 +45,6 @@ class MultiDecorate(BaseDecorator):
             func = i(func)
         return func
 
-class ReturnString(BaseDecorator):
-    def __call__(self, *args, **kwargs):
-        result = BaseDecorator.__call__(self, *args, **kwargs)
-        return str(result).encode('utf-8')
-
 def get_slice(str, start, end):
     if end > len(str) - 1:
         return str[start:]
@@ -86,18 +52,3 @@ def get_slice(str, start, end):
         start = 0
     return str[start:end]
 
-def slugify(string):
-    result = string.lower().expandtabs().split()
-    return str.join('-', result)
-
-def common_keys(dict1, dict2): return set(dict1) & set(dict2)
-    
-def dict_set(dict, key, val): dict[key] = val
-    
-def merge_dict(srcdict, targetdict, updfunc=dict_set):
-    '''copy values from srcdict to targetdict where they share keys'''
-    [updfunc(targetdict, key, srcdict[key]) for key in common_keys(srcdict, targetdict) ]
-    
-
-def both(a, b): return ( a and b )
-def either(a, b): return ( a or b )
