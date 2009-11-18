474de220914528979fb40364044da32b90ba2868
edwlan <edwlan>
2009-11-18 22:05:47 +0000

    'fixed some ordering problems'

--- emen2/testc.py
+++ emen2/testc.py
@@ -1,9 +1,10 @@
 import emen2.globalns
+import emen2.config.config
+parser = emen2.config.config.DBOptions()
+parser.parse_args()
 g = emen2.globalns.GlobalNamespace('')
 import emen2.Database.DBProxy
 
-parser = emen2.config.config.DBOptions()
-parser.parse_args()
 
 db = emen2.Database.DBProxy.DBProxy()
 
