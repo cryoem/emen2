0baf4dc466f66f47556c0d896bddf75431946bc7
edwlan <edwlan>
2010-08-12 20:39:47 +0000

    ''

--- emen2/__init__.py
+++ emen2/__init__.py
@@ -1,3 +1,9 @@
+import collections
+# add OrderedDict from py2.7 to collections
+if not hasattr('collections', 'OrderedDict'):
+	import util.ordereddict
+del collections
+
 import db.proxy
 def opendb():
 	return db.proxy.DBProxy()
