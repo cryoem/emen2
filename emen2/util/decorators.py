bd8371ef67fc42aef691b61b57d5c5f60059f4e6
edwlan <edwlan>
2010-08-18 19:20:10 +0000

    ''

--- emen2/util/decorators.py
+++ emen2/util/decorators.py
@@ -13,3 +13,26 @@ class callonget(object):
 instonget = callonget
 
 
+class _Null: pass
+def cast_arguments(*postypes, **kwtypes):
+	def _func(func):
+		@functools.wraps(func)
+		def _inner(*args, **kwargs):
+			out = []
+			for typ, arg in itertools.izip_longest(postypes, args, fillvalue=_Null):
+				if arg != _Null:
+					if typ != _Null:
+						arg = typ(arg)
+					out.append(arg)
+			for k,v in kwargs.iteritems():
+				typ = kwtypes.get(k, _Null)
+				if typ != _Null:
+					kwargs[k] = typ(kwargs[k])
+			g.debug( args, kwargs )
+			return func(*args, **kwargs)
+		return _inner
+	return _func
+
+
+
+
