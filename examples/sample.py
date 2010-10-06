481b21614ea9117fe95437592a6ab0a60aea8a12
Steve Ludtke <sludtke@bcm.edu>
2010-10-06 19:56:37 +0000

    incomplete first example

---
+++ examples/sample.py
@@ -0,0 +1,18 @@
+#!/bin/env python
+# This is an XMLRPC test script which can be customized for a variety of simple tasks
+# call it as sample.py <username>
+
+import sys
+import emen2.clients.emen2client
+
+db = emen2.clients.emen2client.opendb(username=sys.argv[1])
+
+#print db.getrecord(0)
+
+# Example 1 - query new subprojects by date
+def subprojbydate(db):
+	
+
+
+def histogram
+
