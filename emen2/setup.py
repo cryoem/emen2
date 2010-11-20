bcdf7ee9ad94cd57a436ee4a2413492814b04ef0
Ian Rees <ian.rees@bcm.edu>
2010-11-20 07:25:44 +0000

    *** empty log message ***

---
+++ emen2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+	name='emen2',
+	version='2.0',
+	py_modules=[
+		'emen2',
+		'emen2.db',
+		'emen2.web',
+		'emen2.web.resources',
+		'emen2.web.views',
+		'emen2.skeleton',
+		'emen2.clients',
+		'emen2.clients.emdash',
+		'emen2.clients.emdash.models',
+		'emen2.clients.emdash.threads',
+		'emen2.clients.emdash.ui'
+		],
+	package_dir={'':'..'}
+	)
