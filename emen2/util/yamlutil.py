0bb30c959fc7a63b760c9253445820718607e2f5
Ian Rees <ian.rees@bcm.edu>
2010-11-20 06:17:38 +0000

    reorg

---
+++ emen2/util/yamlutil.py
@@ -0,0 +1,50 @@
+# $Id$
+
+from __future__ import with_statement, print_function
+import yaml
+import emen2.util.jsonutil
+from emen2.db.config import g, DBOptions
+import getpass
+
+#g.log.set_state('LOG_CRITICAL')
+parser = DBOptions()
+parser.add_option('--flowstyle', dest='flowstyle', action='store', default='full', help='yaml flowstyle to use: ')
+parser.add_option('-f', '--file', dest='file', action='store')
+parser.add_option('-k', '--key', dest='keys', action='append')
+parser.add_option('-w', '--write', dest='ofile', action='store')
+parser.add_option('-g', '--get', dest='get', action='store', help='get value')
+parser.add_option('-i', '--inline', dest='inline', action='store_true', help='change inline', default=False)
+parser.add_option('-j', '--json', dest='json', action='store_true', help='return value as json (only for -g)', default=False)
+parser.add_option('', '--interactive', dest='json', action='store_true', help='return value as json (only for -g)', default=False)
+v, args = parser.parse_args(lc=False)
+parser.load_config(loglevel='LOG_CRITICAL')
+
+kwargs = {}
+if v.flowstyle.isdigit(): v.flowstyle = int(v.flowstyle)
+elif v.flowstyle.lower() == 'full': kwargs['fs'] = 0
+elif v.flowstyle.lower() in ('medium','none'): kwargs['fs'] = None
+elif v.flowstyle.lower() == 'compact': kwargs['fs'] = 1
+
+if v.file is not None: kwargs['file'] = v.file
+if v.keys is not None: kwargs['kg'] = v.keys
+if v.set:
+	if len(args) != 2: raise ValueError, 'wrong number of arguments'
+	g.setattr(args[0], yaml.safe_load(args[1]))
+
+if v.get:
+	val = g.getattr(v.get)
+	if v.json: print(emen2.util.jsonutil.encode(val))
+	else: print(val)
+
+else:
+	out = g.to_yaml(**kwargs)
+	if v.ofile and v.file is None:
+		with file(v.ofile, 'w') as f:
+			f.write(out)
+	elif v.set and v.file and v.inline:
+		with file(v.file, 'w') as f:
+			f.write(out)
+	else:
+		print(out)
+
+__version__ = "$Revision$".split(":")[1][:-1].strip()
---
+++ emen2/web/server.py
@@ -0,0 +1,124 @@
+# $Id$
+
+import sys
+import thread
+
+from twisted.internet import reactor
+from twisted.web import static, server
+from twisted.web.resource import Resource
+
+
+try:
+	from twisted.internet import ssl
+except:
+	print "No SSL support"
+	ssl = None
+
+
+import emen2.web.resources.threadpool
+import emen2.db.config
+g = emen2.db.config.g()
+
+if not g.getattr('CONFIG_LOADED', False):
+	try:
+		parser = emen2.db.config.DBOptions()
+		parser.parse_args()
+	except:
+		raise
+		pass
+
+
+def load_resources(root, resources):
+	for path, resource in resources.items():
+		level = 'LOG_INIT'
+		msg = ['RESOURCE', 'LOADED:', '%s to %s']
+		try:
+			root.putChild(path, resource)
+		except:
+			msg[1] = 'FAILED.'
+			level = 'LOG_CRITICAL'
+			del msg[2]
+		else:
+			msg[2] %= (path, resource)
+		# g.log.msg(level,' '.join(msg))
+
+
+def interact():
+	while True:
+		a.interact()
+		exit = raw_input('respawn [Y/n]? ').strip().lower() or 'y'
+		if exit[0] == 'n':
+				thread.interrupt_main()
+				return
+
+
+
+
+class RootResource(Resource):
+	def getChild(self, path, request):
+		print "->", path
+		return self.children.get(request.path.split("/")[0], self)
+
+
+
+def inithttpd():
+	import emen2.web.resources.uploadresource
+	import emen2.web.resources.downloadresource
+	import emen2.web.resources.publicresource
+	import emen2.web.resources.rpcresource
+	import emen2.web.resources.jsonrpcresource
+	# ed: added later
+	# import emen2.web.resources.eman2resource
+
+	import emen2.web.views
+	import emen2.web.view
+	import emen2.web.viewloader
+
+	emen2.web.viewloader.load_views()
+	redirects = g.getattr('REDIRECTS', {})
+	emen2.web.viewloader.load_redirects(redirects)
+	emen2.web.viewloader.routes_from_g()
+
+	root = emen2.web.resources.publicresource.PublicView()
+
+	resources = {
+		'favicon.ico': static.File(emen2.db.config.get_filename('emen2', 'web/static/favicon.ico')),
+		'robots.txt': static.File(emen2.db.config.get_filename('emen2', 'web/static/robots.txt')),
+		'static': static.File(emen2.db.config.get_filename('emen2', 'web/static')),
+		'download': emen2.web.resources.downloadresource.DownloadResource(),
+		'upload': emen2.web.resources.uploadresource.UploadResource(),
+		'RPC2': emen2.web.resources.rpcresource.RPCResource(format="xmlrpc"),
+		'json': emen2.web.resources.rpcresource.RPCResource(format="json"),
+		'jsonrpc': emen2.web.resources.jsonrpcresource.jsonrpc()
+	}
+
+	# load EMEN2 resource
+	try:
+		import EMAN2
+	except:
+		EMAN2 = False
+	if EMAN2:
+		import emen2.web.resources.eman2resource
+		resources.update(eman2 = emen2.web.resources.eman2resource.EMAN2BoxResource())
+
+
+	load_resources(root, resources)
+	rr = server.Site(root)
+	reactor.listenTCP(g.EMEN2PORT, rr)
+
+	if g.EMEN2HTTPS and ssl:
+		reactor.listenSSL(g.EMEN2PORT_HTTPS, server.Site(root), ssl.DefaultOpenSSLContextFactory(os.path.join(g.SSLPATH, "server.key"), os.path.join(g.SSLPATH, "server.crt")))
+
+	reactor.suggestThreadPoolSize(g.NUMTHREADS)
+
+	g.log.msg(g.LOG_INIT, 'Listening on port %d ...' % g.EMEN2PORT)
+	g._locked = True
+	reactor.run()
+
+
+
+if __name__ == "__main__":
+	inithttpd()
+
+
+__version__ = "$Revision$".split(":")[1][:-1].strip()
