3a41d0f263f8a51d50e011e020203684632f66f6
Ian Rees <ian.rees@bcm.edu>
2010-11-20 06:13:14 +0000

    *** empty log message ***

---
+++ testscripts/getschema.py
@@ -0,0 +1,63 @@
+# $Id$
+
+import json
+import xmlrpclib
+import urllib2
+
+import emen2.db.config
+g = emen2.db.config.g()
+
+if not g.getattr('CONFIG_LOADED', False):
+	try:
+		parser = emen2.db.config.DBOptions()
+		parser.parse_args()
+	except:
+		raise
+
+import emen2
+import getpass
+db = emen2.opendb()
+ruser = user = g.getprivate('USERNAME') or 'root'
+rpasswd = passwd = g.getprivate('ROOTPW')
+if not passwd:
+	passwd = getpass.getpass()
+db._login(user, passwd)
+
+# if not raw_input('same remote credentials? ').lower().startswith('y'):
+# 	print '''\
+# Change:
+# 	1. username?
+# 	2. password?
+# ?''',
+# 	if raw_input('').strip() == '1': ruser = raw_input('remote username? ')
+# 	else: rpasswd = raw_input('remote password? ')
+
+rdb = xmlrpclib.ServerProxy('http://ncmi.bcm.tmc.edu/challenge/RPC2/')
+# ctxid = rdb._login(ruser, rpasswd)
+
+g.log_info('updating paramdefs')
+paramdefs = rdb.getparamdefnames()
+paramdefs = (rdb.getparamdef(x) for x in paramdefs if not x.lower().startswith('file'))
+for pd in paramdefs: db.putparamdef(pd)
+g.log_info('updating recorddefs')
+recorddefs = rdb.getrecorddefnames()
+recorddefs = (rdb.getrecorddef(x) for x in recorddefs if not x.lower().startswith('file'))
+for rd in recorddefs: db.putrecorddef(rd)
+
+def puttree(keytype, root):
+	jsonrequest = json.dumps( dict(keys=root, keytype=keytype, recurse=10000) )
+	jsonresponse = urllib2.urlopen('http://ncmi.bcm.tmc.edu/challenge/json/getchildtree', jsonrequest)
+	def closure():
+		tree = json.load(jsonresponse)
+		return ( (elem,child) for elem in tree for child in tree[elem] )
+	tree = closure()
+	for elem, child in tree: db.pclink(elem, child, keytype=keytype)
+
+g.log_info('setting paramdef tree')
+puttree('paramdef', 'root_parameter')
+
+g.log_info('setting recorddef tree')
+puttree('recorddef', 'root_protocol')
+
+g.log_info('done')
+__version__ = "$Revision$".split(":")[1][:-1].strip()
