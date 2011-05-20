601e2299faf328f7b214c84389cca44d42b3ee49
Ian Rees <ian.rees@bcm.edu>
2011-05-20 12:24:28 +0000

    *** empty log message ***

--- setup.py
+++ setup.py
@@ -11,42 +11,9 @@ URLMAP = {
 	"2.0rc2": "software_107",
 	"2.0rc3": "software_108",
 	"2.0rc4": "software_110",	
-	"2.0rc5": "software_113"
+	"2.0rc5": "software_113",
+	"2.0rc6": "software_114"	
 }
-
-SCPBASE="10.10.9.104:/homes/www/Zope-2.7.0/var/extdata/reposit/ncmi/software/EMEN2"
-
-def upload(v):
-	infile = "dist/emen2-%s.tar.gz"%v
-	dest = "%s/%s"%(SCPBASE, URLMAP[v])
-	p = subprocess.Popen(["scp", infile, dest])
-	sts = os.waitpid(p.pid, 0)
-
-# def filterwalk(path, filetypes=None):
-# 	filetypes = filetypes or []
-# 	ret = []
-# 	for i in os.walk(path):
-# 
-# 		base = i[0]
-# 		base = i[0].split(os.sep)[1:]
-# 		if base:
-# 			base = os.path.join(*base)
-# 		else:
-# 			base = ''
-# 
-# 		for j in i[2]:
-# 			ext = os.path.splitext(j)[-1]
-# 			if filetypes and ext not in filetypes:
-# 				continue
-# 			ret.append(os.path.join(base, j))
-# 
-# 	return ret
-# 
-# static_files = \
-# 	filterwalk('emen2/static', filetypes=['.png', '.gif', '.css', '.js', '.jpg', '.ico', '.txt']) +  \
-# 	filterwalk('emen2/templates', filetypes=['.mako']) + \
-# 	filterwalk('emen2/', filetypes=['.json'])
-
 
 if __name__ == "__main__":
 	setup(
