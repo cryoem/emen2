358b75d35fb040dca26309fe77752fa753285dce
edwlan <edwlan>
2011-06-13 20:05:13 +0000

    'emen2/web/server.py'

--- emen2/web/viewloader.py
+++ emen2/web/viewloader.py
@@ -12,75 +12,79 @@ import emen2.db.config
 g = emen2.db.config.g()
 
 
-def view_callback(pwd, pth, mtch, name, ext, failures=None):
-	if pwd[0] not in sys.path:
-		sys.path.append(pwd[0])
-	if not hasattr(failures, 'append'): 
-		failures = []
-	assert ext == mtch#:
-	filpath = os.path.join(pwd[0], name)
-	data = emen2.util.fileops.openreadclose(filpath+ext)
-	viewname = os.path.join(pwd[0], name).replace(pth,'')
-	level = 'DEBUG'
-	msg = ["VIEW", "LOADED:"]
-	try:
-		__import__(name)
-	except BaseException, e:
-		g.info(e)
-		level = 'ERROR'
-		msg[1] = "FAILED:"
-		failures.append(viewname)
+class ViewLoader(object):
+	templates = g.claim('templates')
+	templatepaths = g.claim('paths.TEMPLATEPATHS', [])
+	viewpaths = g.claim('paths.VIEWPATHS', [])
+	routing_table = g.claim('ROUTING', {})
+	redirects = g.claim('REDIRECTS', {})
 
-	msg.append(filpath+ext)
-	g.log.msg(level, ' '.join(msg))
-
-
-get_views = emen2.util.fileops.walk_paths('.py', view_callback)
-
-
-def routes_from_g():
-	routing_table = g.getattr('ROUTING', {})
-	router = emen2.web.routing.URLRegistry()
-	for key, value in routing_table.iteritems():
-		for name, regex in value.iteritems():
-			view = router.get(key)
-			if view:
-				view.add_matcher(name, regex, view.get_callback('main'))
-
-
-
-def load_views(failures=None):
-	g.templates = emen2.web.templating.TemplateFactory('mako', emen2.web.templating.MakoTemplateEngine())
-	#ed: deprecated -- use TEMPLATEPATHS
-	# r = reversed(getattr(g.paths, 'TEMPLATEDIRS', []))
-	# emen2.web.templating.get_templates(r, failures=failures)
-	# new name
-	r = reversed(getattr(g.paths, 'TEMPLATEPATHS', []))
-	emen2.web.templating.get_templates(r, failures=failures)
-	get_views(getattr(g.paths, 'VIEWPATHS', []))
-
-
-
-def load_redirects(dict_):
-	for fro,v in dict_.iteritems():
-		to, kwargs = v
-		emen2.web.resources.publicresource.PublicView.register_redirect(fro, to, **kwargs)
-
-
-
-def reload_views(view=None):
-	reload(view)
-	failures = []
-	load_views(failures=failures)
-	if view != None: values = [emen2.web.routing.URLRegistry.URLRegistry[view]]
-	else: values = emen2.web.routing.URLRegistry.URLRegistry.values()
-	for view in values:
+	def view_callback(self, pwd, pth, mtch, name, ext, failures=None):
+		if pwd[0] not in sys.path:
+			sys.path.append(pwd[0])
+		if not hasattr(failures, 'append'):
+			failures = []
+		assert ext == mtch#:
+		filpath = os.path.join(pwd[0], name)
+		data = emen2.util.fileops.openreadclose(filpath+ext)
+		viewname = os.path.join(pwd[0], name).replace(pth,'')
+		level = 'DEBUG'
+		msg = ["VIEW", "LOADED:"]
 		try:
-			view = view._callback.__module__
-			exec 'import %s;reload(%s)' % (view,view)
-		except:
-			failures.append(str(view))
-	return failures
+			__import__(name)
+		except BaseException, e:
+			g.info(e)
+			level = 'ERROR'
+			msg[1] = "FAILED:"
+			failures.append(viewname)
+
+		msg.append(filpath+ext)
+		g.log.msg(level, ' '.join(msg))
+
+
+	def __init__(self):
+		self.get_views = emen2.util.fileops.walk_paths('.py', self.view_callback)
+		self.router = emen2.web.routing.URLRegistry()
+
+	def routes_from_g(self):
+		for key, value in self.routing_table.iteritems():
+			for name, regex in value.iteritems():
+				view = self.router.get(key)
+				if view:
+					view.add_matcher(name, regex, view.get_callback('main'))
+
+
+	def load_templates(self, failures=None):
+		r = reversed(self.templatepaths)
+		emen2.web.templating.get_templates(r, failures=failures)
+
+
+	def load_views(self, failures=None):
+		self.get_views(self.viewpaths)
+
+
+
+	def load_redirects(self):
+		for fro,v in self.redirects.iteritems():
+			to, kwargs = v
+			emen2.web.resources.publicresource.PublicView.register_redirect(fro, to, **kwargs)
+
+
+
+	def reload_views(self, view=None):
+		reload(view)
+		failures = []
+		self.load_templates(failures=failures)
+		self.load_views(failures=failures)
+		if view != None: values = [emen2.web.routing.URLRegistry.URLRegistry[view]]
+		else: values = emen2.web.routing.URLRegistry.URLRegistry.values()
+		for view in values:
+			try:
+				view = view._callback.__module__
+				exec 'import %s;reload(%s)' % (view,view)
+			except:
+				failures.append(str(view))
+		return failures
 
 
 
