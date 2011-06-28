448b40cadb3967079f3a97095552e685fe038a0a
edwlan <edwlan>
2011-06-28 01:15:59 +0000

    ''

--- emen2/web/view.py
+++ emen2/web/view.py
@@ -28,6 +28,7 @@ import emen2.web.templating
 
 from emen2.util.listops import adjust
 from emen2.web import routing
+import emen2.web.extfile
 
 import emen2.db.config
 g = emen2.db.config.g()
@@ -36,93 +37,6 @@ __all__ = ['View', 'ViewPlugin', 'AdminView', 'AuthView', 'Page']
 
 
 
-
-class ExtFile(object):
-	def _register(registry):
-		@staticmethod
-		def _inner(name, bases, dict):
-			newcls = type(name, bases, dict)
-			registry[name] = newcls
-			return newcls
-		return _inner
-
-	css_registry = {}
-	registercss = _register(css_registry)
-
-	js_registry = {}
-	registerjs = _register(js_registry)
-
-
-	def addfile(self, f):
-		self.files.append('%s/static/%s'%(g.EMEN2WEBROOT, f)) #, g.VERSION
-
-	@property
-	def files(self):
-		return self._files
-
-	@files.setter
-	def files(self, value):
-		if not hasattr(value, '__iter__'):
-			value = [value]
-		self._files.extend(value)
-
-	def __init__(self, dbtree):
-		self._files = []
-		self.dbtree = dbtree
-		self.init()
-
-	def init(self): pass
-
-	def __iter__(self):
-		return iter(self._files)
-
-
-
-# ian: todo: optimize this some in the future; not every page needs all the files
-class BaseJS(ExtFile):
-	def init(self):
-		super(BaseJS, self).init()
-		addfiles = [
-			'jquery/jquery.js',
-			'jquery/jquery-ui.js',
-			'jquery/jquery.json.js',
-			'jquery/jquery.timeago.js',
-			"comments.js",
-			"edit.js",
-			"editdefs.js",
-			"file.js",
-			"find.js",
-			"permission.js",
-			"query.js",
-			"relationship.js",
-			"table.js",
-			"tile.js",
-			"record.js",
-			"util.js"
-			]
-
-		self.files = [self.dbtree.reverse('TemplateRender', t='/js/settings.js')]
-		for i in addfiles:
-			self.files.append('%s/static-%s/js/%s'%(g.EMEN2WEBROOT, emen2.VERSION, i))
-
-
-class BaseCSS(ExtFile):
-	def init(self):
-		super(BaseCSS, self).init()
-		addfiles = [
-			'custom-theme/jquery-ui-1.8.2.custom.css',
-			'base.css',
-			'style.css',
-			'boxer.css'
-			#'colors.css',
-			#'calendar.css'
-		]
-
-		self.files = []
-		for i in addfiles:
-			self.files.append('%s/static-%s/css/%s'%(g.EMEN2WEBROOT, emen2.VERSION, i))
-
-		self.files.append(self.dbtree.reverse('TemplateRender', t='/css/map.css'))
 
 
 ############ ############ ############
@@ -171,7 +85,7 @@ class ViewContext(collections.MutableMapping):
 
 
 
-###NOTE: This class should not access the db in anyway, such activity is carried out by
+###NOTE: This class should not access the db in any way, such activity is carried out by
 ###		the View class below.
 class _View(object):
 	'''Base Class for views, sets up the instance variables for the class
@@ -206,8 +120,8 @@ class _View(object):
 
 	ctxt = property(lambda self: self.get_context())
 	dbtree = property(lambda self: self.__dbtree)
-	js_files = BaseJS
-	css_files = BaseCSS
+	js_files = emen2.web.extfile.BaseJS
+	css_files = emen2.web.extfile.BaseCSS
 	page = None
 
 	def __settitle(self, t):
