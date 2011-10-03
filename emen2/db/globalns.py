07d50785ce7b2720577afa2a65bb5dfdc6ece550
edwlan <edwlan>
2011-10-03 16:46:43 +0000

    ''

--- emen2/db/globalns.py
+++ emen2/db/globalns.py
@@ -4,23 +4,30 @@ from __future__ import with_statement
 
 import itertools
 import collections
-def dict_merge(dct1, dct2):
-	commonkeys = set(dct1) & set(dct2)
-	newkeys = set(dct2) - commonkeys
+def dict_merge(dct1, dct2, dict_type, root):
+	'''Given two dictionaries, recursively merge the second into the first
 
-	for key in commonkeys:
-		olditem = dct1[key]
-		newitem = dct2[key]
-		if hasattr(newitem, 'items') and hasattr(olditem, 'items'):
-			dict_merge(olditem, newitem)
-		#elif hasattr(olditem, 'update') and hasattr(newitem, '__iter__'):
-		#	olditem.update(newitem)
-		#elif hasattr(olditem, 'extend') and hasattr(newitem, '__iter__'):
-		#	olditem.extend(newitem)
+	i.e:
+	>>> dict_merge({1: 1, 2: 2, 3: 3, 5: {1: 1, 2: 2}}, {1: 2, 4: 4, 5: {1: 3, 6: 6}})
+	{1: 2, 2: 2, 3: 3, 4: 4, 5: {1: 3, 2: 2, 6: 6}}
+	'''
+
+	result = {}
+
+	for key in set(dct1) | set(dct2): # iterate through both dicts
+		olditem = dct1.get(key, {})
+		newitem = dct2.get(key)
+		if hasattr(newitem, 'items'):
+			print 'halo'
+			item = dict_type(values=dict_merge(olditem, newitem, dict_type, root)) # if both items are dictionaries, merge them
+			item._root = root
+			result[key] = item
+		elif key in dct2:
+			result[key] = dct2[key] # if both have the item, update dct1
 		else:
-			dct1[key] = dct2[key]
-	dct1.update( (k,dct2[k]) for k in newkeys )
-	return dct1
+			result[key] = olditem
+
+	return result
 
 
 class _Null: pass
@@ -33,9 +40,10 @@ class Hier(collections.MutableMapping):
 
 	@classmethod
 	def from_dict(cls, dct):
-		#cls.init()
-		dict_merge(cls.hier, dct)
-		return cls()
+		self = cls()
+		self._root = self
+		cls.hier = dict_merge(cls.hier, dct, dict_type=cls, root=self)
+		return self
 
 	#######################
 	# Instance Attributes #
@@ -44,19 +52,23 @@ class Hier(collections.MutableMapping):
 	def to_dict(self):
 		return self._values
 
-	def __init__(self, name=''):
-		self._values = self.hier
+	def __init__(self, name='', values=None):
+		# "name" and "values" are for developers only
 		self._create = False
-
 		self._name = name
+		self._root = self
 
-		for segment in self._name.split('.'):
-			item = self._values.get(segment)
-			if hasattr(item, 'items'):
-				#print item
-				self._values = item
-			elif name != '':
-				raise ValueError('no such item: %r' % self._name)
+		# find the value associated with the current object
+		self._values = self.hier
+		if values is not None and '.' not in name:
+			self._values = values
+		else:
+			for segment in self._name.split('.'):
+				item = self._values.get(segment)
+				if hasattr(item, 'items'):
+					self._values = item
+				elif name != '':
+					raise ValueError('no such item: %r' % self._name)
 
 	def __repr__(self):
 		return '%s().from_dict(%r)' % (self.__class__.__name__, self._values)
@@ -90,9 +102,9 @@ class Hier(collections.MutableMapping):
 		result = self._values.get(name, _Null)
 		if result is _Null and default is not _Null:
 			result = default
-		elif hasattr(result, 'items'):
-			if prefix == '': result = self.__class__(name)
-			else: result = self.__class__('%s.%s' % (prefix, name))
+		#elif hasattr(result, 'items'):
+		#	if prefix == '': result = self.__class__(name)
+		#	else: result = self.__class__('%s.%s' % (prefix, name))
 		return result
 
 	def __getitem__(self, name):
@@ -172,6 +184,9 @@ log = LoggerStub()
 
 inst = lambda x:x()
 class GlobalNamespace(Hier):
+	def __init__(self, *a, **k):
+		Hier.__init__(self, *a, **k)
+		self.__options = collections.defaultdict(set)
 
 	def claim(self, name, default=None, validator=None):
 		return Claim(self, name, default, validator)
@@ -179,19 +194,17 @@ class GlobalNamespace(Hier):
 		return Watch(self, name, default)
 
 	_events = collections.defaultdict(list)
-	@classmethod
-	def register_event(cls, var):
+	def register_event(self, var):
 		'''events shouldn't modify the state of the GlobalNamespace!!!'''
+		var = '.'.join([self._name, var])
 		def _inner(cb):
-			cls._events[var].append(cb)
-			if var in cls.__vardict:
-				cb(cls.__vardict[var], read=True)
+			self._events[var].append(cb)
 			return cb
 		return _inner
 
-	@classmethod
-	def _trigger_event(cls, var, value, read=False, write=False):
-		for cb in cls._events.get(var, []):
+	def _trigger_event(self, var, value, read=False, write=False):
+		var = '.'.join([self._name, var])
+		for cb in self._events.get(var, []):
 			cb(value, read=read, write=write)
 
 
@@ -234,12 +247,11 @@ class GlobalNamespace(Hier):
 
 	__yaml_keys = collections.defaultdict(list)
 	__yaml_files = collections.defaultdict(list)
-	__options = collections.defaultdict(set)
 	__all__ = []
 
 	@property
 	def EMEN2DBHOME(self):
-		return self.__vardict.get('EMEN2DBHOME')
+		return self.getattr('EMEN2DBHOME')
 	@EMEN2DBHOME.setter
 	def EMEN2DBHOME(self, value):
 		self.setattr('EMEN2DBHOME', value)
@@ -274,9 +286,9 @@ class GlobalNamespace(Hier):
 
 
 	def __exit__(self, exc_type, exc_value, tb):
-		newitems = set(self.__tmpdict) - set(self.__vardict)
+		newitems = set(self.__tmpdict) - set(self)
 		if newitems:
-			self.__vardict.update(x for x in self.__tmpdict.iteritems() if x[0] in newitems)
+			self.update(x for x in self.__tmpdict.iteritems() if x[0] in newitems)
 			skipped = set(self.__tmpdict) - newitems
 			if skipped: self.log.msg('WARNING', 'skipped items: %r' % skipped)
 		del self.__tmpdict
@@ -353,14 +365,13 @@ class GlobalNamespace(Hier):
 			self._create = True
 			for key in data:
 				b = data[key]
-				pref = ''.join(b.pop('prefix',[])) # get the prefix for the current toplevel dictionary
 				options = b.pop('options', {})	  # get options for the dictionary
 				self.__yaml_files[fn].append(key)
 
+				self.from_dict(data)
 				for key2, value in b.iteritems():
 					self.__yaml_keys[key].append(key2)
-					#self.setattr('.'.join([key,key2]), value, options)
-			self.from_dict(data)
+					self.setattr('.'.join([key,key2]), value, options)
 
 			# load alternate config files
 			# for fn in self.paths.CONFIGFILES:
@@ -405,19 +416,6 @@ class GlobalNamespace(Hier):
 		return dict(self._values)
 
 
-	#@classmethod
-	#def setattr(self, name, value):
-	#	self.__addattr(name, value)
-
-
-	#def __setattr__(self, name, value):
-	#	self._trigger_event(name, value, write=True)
-	#	res = getattr(self.__class__, name, None)
-	#	if name.startswith('_') or hasattr(res, '__set__'):
-	#		object.__setattr__(self, name, value)
-	#	else:
-	#		self.setattr(name, value)
-
 	def __getitem__(self, name):
 		if not hasattr(name, 'split'):
 			name = '.'.join(name)
@@ -436,10 +434,17 @@ class GlobalNamespace(Hier):
 
 		self.check_locked()
 		if name.startswith('_') or not self.__locked:
-			if options is not None:
-				for option in options:
-					self.__options[option].add(name)
 			Hier.setattr(self, name, value)
+			if options is not None:
+				print 'options given:', options
+				parent, key = name.rpartition('.')[::2]
+				print 'added to:', parent, 'for', key
+				parent = self._root.getattr(parent)
+				for option in options:
+					parent.__options[option].add(key)
+					print parent.__options
+
+
 		else: raise LockedNamespaceError, 'cannot change locked namespace'
 
 
@@ -448,33 +453,30 @@ class GlobalNamespace(Hier):
 		if name.endswith('___'):
 			name = name.partition('___')[-1]
 		if self.__options.has_key('private'):
-			if name in self.__options['private']:
+			parent, key = name.rpartition('.')[::2]
+			parent = self._root.getattr(parent)
+			if key in parent.__options['private']:
 				return default
 		result = Hier.getattr(self, name, default=default, *args, **kwargs)
 		self._trigger_event(name, result, read=True)
 		return result
 
 
-	@classmethod
-	def keys(cls):
-		private = cls.__options['private']
-		return [k for k in cls.__vardict.keys() if k not in private]
+	def keys(self):
+		private = self.__options['private']
+		return [k for k in Hier.keys(self) if k not in private]
 
 
-	@classmethod
-	def getprivate(cls, name):
+	def getprivate(self, name):
 		if name.startswith('___'):
 			name = name.partition('___')[-1]
 		result = Hier.getattr(self, name)
 		return result
 
 
-	def update(self, dict):
-		self.__vardict.update(dict)
-
 
 	def reset(self):
-		self.__class__.__vardict = {}
+		self.clear()
 
 
 
