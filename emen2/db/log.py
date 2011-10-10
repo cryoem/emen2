ff7c1e06257d93c23627fe67b1982b981426625b
Ian Rees <ian.rees@bcm.edu>
2011-10-10 23:07:25 +0000

    *** empty log message ***

---
+++ emen2/db/log.py
@@ -0,0 +1,41 @@
+# $Id$
+'''EMEN2 logging
+
+Functions:
+	msg
+	info
+	init
+	error
+	warn
+	debug
+	security
+	index
+	commit
+'''
+
+# Logging methods
+import functools
+
+logger = None
+
+def msg(level='INFO', msg=''):
+	if 'DEBUG' in level:
+		return
+	# if logger:
+	# 	return logger.msg(msg)
+	print '%s: %s'%(level, msg)
+
+# Argghgh..
+msg_forwards = msg
+def msg_backwards(msg='', level='INFO'):
+	return msg_forwards(msg=msg, level=level)
+
+# Aliases
+info = functools.partial(msg_backwards, level='INFO')
+init = functools.partial(msg_backwards, level='INIT')
+error = functools.partial(msg_backwards, level='ERROR')
+warn = functools.partial(msg_backwards, level='WARN')
+debug = functools.partial(msg_backwards, level='DEBUG')
+security = functools.partial(msg_backwards, level='SECURITY')
+index = functools.partial(msg_backwards, level='INDEX')
+commit = functools.partial(msg_backwards, level='COMMIT')
