e79e931059dcebf17846ad76c4308cedcd265ecd
Ian Rees <ian.rees@bcm.edu>
2011-12-11 05:43:55 +0000

    *** empty log message ***

--- testscripts/fix_dates.py
+++ testscripts/fix_dates.py
@@ -1,74 +1,105 @@
 import datetime
+import dateutil
+import dateutil.parser
+import dateutil.tz
+
+tzutc = dateutil.tz.tzutc()
+tzlocal = dateutil.tz.gettz()
+default = datetime.datetime(2011, 1, 1)
+mintime = datetime.datetime(2000, 1, 1)
+
 
 import emen2.db
 db = emen2.db.opendb(admin=True)
 
-TIMESTR = ['%Y/%m/%d %H:%M:%S', '%Y/%m/%d', '%Y']
-def parse(d, tz='Z'):
-	p = None
-	for t in TIMESTR:
+def parseutc(d):
+	try:
+		t = dateutil.parser.parse(d, default=default)
+	except ValueError, e:
+		print "Couldn't parse:", d, e
+		return
+	if not t.tzinfo:
+		t = t.replace(tzinfo=tzlocal)
+	t = t.astimezone(tzutc)
+	# print "Converted to UTC       :", t.isoformat(), "\t\t", d	
+	return t.isoformat()
+
+def parselocal(d):
+	try:
+		t = dateutil.parser.parse(d, default=default)
+	except ValueError, e:
+		print "Couldn't parse:", d, e
+		return
+	if not t.tzinfo:
+		t = t.replace(tzinfo=tzlocal)
+	# print "Converted to UTC+offset:", t.isoformat(), "\t\t", d	
+	return t.isoformat()
+
+	# t = dateutil.parser.parse(d, default=default).replace(tzinfo=tzlocal)
+	# print "Converted to UTC+offset:", t.isoformat(), "\t\t", d
+	# return t.isoformat()
+
+
+# print "Test:"
+# parseutc('2009')
+# parselocal('2009')
+# parseutc('2009/02/03')
+# parselocal('2009/02/03')
+# parselocal('2009/02/03 02:03:04')
+# 
+# import sys
+# sys.exit(0)
+
+def updatebt(btree, txn):
+	for name, item in btree.items(txn=txn):
+		# print name
+		ct = item.__dict__.get('creationtime') or '2001/01/01'
+		item.__dict__['creationtime'] = parseutc(ct)
+		item.__dict__['modifytime'] = parseutc(item.get('modifytime') or ct)
 		try:
-			p = datetime.datetime.strptime(d, t)
+			btree.put(name, item, txn=txn)
 		except Exception, e:
-			pass
-		if p:
-			return p.isoformat()+tz
-	if not p:
-		print "Could not parse:", d
-	return d
-	
-
-print "Test:"
-print parse('2009')
-print parse('2009/01/01')
-print parse('2009/01/01 02:03:04')
-
-t = []
-t.append(parse('2009/01/01 02:03:01'))
-t.append(parse('2009/01/01 02:03:02'))
-t.append(parse('2009/01/01 02:03:03', tz='-06:00'))
-t.append(parse('2009/01/01 02:03:04', tz='-06:00'))
-t.append(parse('2009/01/01 02:03:05'))
-
-print sorted(t)
-
-import sys
-sys.exit(0)
-
+			print "Skipped...", e
 		
-with db:
-	
+with db:	
 	pds = set(db.query([['vartype','==','datetime']], keytype='paramdef', count=0)['names'])
 	pds -= set(['creationtime', 'modifytime'])
-	print pds
-	
+	print "Converting timestamps for:", pds
 	
 	ctx = db._ctx
 	txn = db._txn
+	
+	for i in ['user', 'group', 'paramdef', 'recorddef']:
+		updatebt(db._db.bdbs.keytypes[i], txn=txn)
+	
 	#for name in db._db.bdbs.record.keys(txn=txn): 
-	for name in range(1000):
-		print name
+	for name in range(550000):
+		if name % 1000 == 0:
+			print name
+
 		rec = db.getrecord(name)
 		if not rec:
 			continue
 			
 		ct = rec.__dict__['creationtime']
-		rec.__dict__['creationtime'] = parse(ct)
-		rec.__dict__['modifytime'] = parse(rec.get('modifytime') or ct)
+		rec.__dict__['creationtime'] = parseutc(ct)
+		rec.__dict__['modifytime'] = parseutc(rec.get('modifytime') or ct)
 
 		newcomments = []
 		for i in rec.__dict__['comments']:
-			newcomments.append([i[0], parse(i[1]), i[2]])
+			newcomments.append([i[0], parseutc(i[1]), i[2]])
 		rec.__dict__['comments'] = newcomments
 		
 		newhistory = []
 		for i in rec.__dict__['history']:
-			newhistory.append([i[0], parse(i[1]), i[2], i[3]])
+			newhistory.append([i[0], parseutc(i[1]), i[2], i[3]])
 		rec.__dict__['history'] = newhistory
 		
 		for p in pds:
 			if rec.params.get(p):
-				rec.params[p] = parse(rec.params[p])
-				
+				rec.params[p] = parselocal(rec.params[p])
 
+		db._db.bdbs.record.put(rec.name, rec, txn=txn)
+		
+		
 		
--- testscripts/fix_times_utc.py
+++
@@ -1,4 +0,0 @@
-import collections
-import emen2.db
-db = emen2.db.opendb()
-
