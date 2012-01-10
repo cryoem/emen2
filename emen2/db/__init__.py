460b9b46bc7259ce617a18e484151d5bb750f8b6
Ian Rees <ian.rees@bcm.edu>
2012-01-10 07:45:26 +0000

    *** empty log message ***

--- emen2/db/__init__.py
+++ emen2/db/__init__.py
@@ -4,7 +4,7 @@
 def opendb(*args, **kwargs):
 	"""Helper method for opening DB with default options"""
 	import config
-	dbo = config.DBOptions(loginopts=True)
+	dbo = config.DBOptions()
 	(options, args) = dbo.parse_args()
 	return dbo.opendb(*args, **kwargs)
 
