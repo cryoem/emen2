a320d754731f4a106d6bc7a203965766910edb01
edwlan <edwlan>
2012-01-27 22:42:45 +0000

    ''

--- emen2/db/log.py
+++ emen2/db/log.py
@@ -36,8 +36,6 @@ class Variables:
 	def init_done(self, value):
 		self._init_done = bool(value)
 
-	LOG_LEVEL = emen2.db.config.get('LOG_LEVEL', None, False)
-	LOGPATH = emen2.db.config.get('paths.LOGPATH', None, False)
 	logger = None
 
 	def log_init(self, done=False):
@@ -51,49 +49,59 @@ class Variables:
 
 log_init = Variables.log_init
 
-log_levels = emen2.util.datastructures.Enum(dict(
-      DEBUG=-1,
-      TXN=1,
-      INIT=2,
-      INFO=3,
-      INDEX=4,
-      COMMIT=5,
-      WEB=6,
-		WARN=8,
-      SECURITY=9,
-      ERROR=10,
-      CRITICAL=11,
-      ALL=0
-))
-
 import twisted.python.log
 
-@twisted.python.log.addObserver
-def emen2_logs(event, *args, **kwargs):
-	if 'level' in event:
-		level = event['level']
-		if level >= log_levels[Variables.LOG_LEVEL]:
-			print log_levels.get_name(event['level']), '::', ' '.join(str(k) for k in event['message'])
-	elif event['isError']:
-		print 'ERROR', '::', ' '.join(str(k) for k in event['message'])
-	else:
-		pass
+class EMEN2LogObserver(object):
+	LOG_LEVEL = emen2.db.config.get('LOG_LEVEL', None, False)
+	LOGPATH = emen2.db.config.get('paths.LOGPATH', None, False)
 
-def EMEN2Logger(object):
-	pass
+	log_levels = emen2.util.datastructures.Enum(dict(
+			DEBUG=-1,
+			TXN=1,
+			INIT=2,
+			INFO=3,
+			INDEX=4,
+			COMMIT=5,
+			WEB=6,
+			WARN=8,
+			SECURITY=9,
+			ERROR=10,
+			CRITICAL=11,
+			ALL=0
+	))
+
+
+	@classmethod
+	def emen2_logs(cls, event): # *args, **kwargs):
+		self = cls()
+		self.process(event)
+
+	def process(self, event, *args, **kwargs):
+		level = event.get('level')
+		if 'level' in event:
+			level = event['level']
+			if level >= self.log_levels[self.LOG_LEVEL]:
+				print self.log_levels.get_name(event['level']), '::', ' '.join(str(k) for k in event['message'])
+		elif event['isError']:
+			print 'ERROR', '::', ' '.join(str(k) for k in event['message'])
+		else:
+			pass
+
+twisted.python.log.addObserver(EMEN2LogObserver.emen2_logs)
 
 def msg(level='INFO', msg=''):
-	#if level in IGNORE:
-	#	return
-	if Variables.logger:
-		twisted.python.log.msg(msg, level=log_levels[level])
-	else:
-		print '%s: %s'%(level, msg)
+	twisted.python.log.msg(msg, level=EMEN2LogObserver.log_levels[level])
 
+def flip(func):
+	@functools.wraps(func)
+	def _inner(self, *args, **kwargs):
+		return func( *args[::-1], **kwargs)
+	return _inner
 # Argghgh..
 msg_forwards = msg
-def msg_backwards(msg='', level='INFO'):
-	return msg_forwards(msg=msg, level=level)
+msg_backwards = flip(msg_forwards)
+#def msg_backwards(msg='', level='INFO'):
+#	return msg_forwards(msg=msg, level=level)
 
 def nothing(*args, **kwargs):
 	pass
@@ -108,9 +116,9 @@ security = functools.partial(msg_backwards, level='SECURITY')
 index = nothing # functools.partial(msg_backwards, level='INDEX')
 commit = functools.partial(msg_backwards, level='COMMIT')
 
-def detach():
-	Variables.logger.capturestdout()
-	Variables.logger.closestdout()
+#def detach():
+#	Variables.logger.capturestdout()
+#	Variables.logger.closestdout()
 
 def print_exception():
 	traceback.print_exc()
