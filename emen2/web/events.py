7467bf25110f8aaaad3c3d44dfe0b6263964443b
edwlan <edwlan>
2012-04-12 21:19:19 +0000

    'documentation work'

--- emen2/web/events.py
+++ emen2/web/events.py
@@ -2,19 +2,24 @@ import functools
 import threading
 from emen2.util import registry
 
+#NOTE: this is probably unnecessary... All it adds is error-handling if the callback is not 
+     # actually callable
 class Callback(object):
+	'''wrap a callback to be associated with an event'''
 	def __init__(self, cb, *args, **kwargs):
 		if args or kwargs:
 			cb = functools.partial(cb, *args, **kwargs)
 		self.cb = cb
 
 	def __call__(self, *args, **kwargs):
-		result = None
+		result = self.cb
 		if callable(self.cb):
 			result = self.cb(*args, **kwargs)
 		return result
 
 class Event(object):
+	'''Holds the callbacks to respond to and the observers to notify on an event'''
+
 	_cb_lock = threading.RLock()
 	_o_lock = threading.RLock()
 	def __init__(self, name):
@@ -23,27 +28,35 @@ class Event(object):
 		self.observers = []
 
 	def add_observer(self, cb, *args, **kwargs):
+		'''Add an observer, it will be called with arguments from *args and **kwargs
+		as well as the result returned by each callback for this event'''
 		with self._o_lock:
 			self.observers.append(functools.partial(cb, *args, **kwargs))
 		return self
 
 	def add_cb(self, cb, *args, **kwargs):
+		'''Add a callback, it will be called with the arguments from *args and **kwargs
+		as well as any additional arguments passed to the __call__ method of this object'''
 		with self._cb_lock:
 			cb = Callback(cb, *args, **kwargs)
 			self.callbacks.append(cb)
 			return cb
 
 	def add_cbs(self, cbs):
+		'''Add many callbacks, argument format: { callback: (args, kwargs) }'''
 		for (cb, (a, kw)) in cbs.iteritems():
 			self.add_cb(cb, *a, **kw)
 		return self
 
 	def remove_cb(self, cb):
+		'''Remove a callback, argument: the callback to be removed'''
 		self.callbacks = [x for x in self.callbacks if x is not cb]
 		return self
 
 	def __call__(self, *args, **kwargs):
+		'''Call each callback and notify every observer about its results'''
 		results = []
+
 		for cb in self.callbacks:
 			result = cb(*args, **kwargs)
 			with self._o_lock:
@@ -54,12 +67,14 @@ class Event(object):
 
 @registry.Registry.setup
 class EventRegistry(registry.Registry):
+	'''Register an event, see emen2.util.registry for documentation'''
 	child_class = Event
 
 	events = {}
-	nullevent = Event('')
+	nullevent = Event('') #Ed: I'm not sure if this is necessary
 
 	def observe_event(self, name, cb, *args, **kwargs):
+		'''Add an observer to an event'''
 		with self._lock:
 			self.events[name].add_observer(cb, *args, **kwargs)
 			return self
