a9a507a7961c4da34273c1cf1a804d2ab8465c1d
Ian Rees <ian.rees@bcm.edu>
2012-07-31 02:06:03 +0000

    *** empty log message ***

--- testscripts/scratch.py
+++
@@ -1,6 +0,0 @@
-import emen2.db
-db = emen2.db.opendb(admin=True)
-with db:
-    # print db.query([['rectype','contains','ddd']])
-    for i in range(3):
-        print db.query([['*','contains','ddd']])
