22be660141647f0a1c87a46d7be1fc9a72a18ff4
Ian Rees <ian.rees@bcm.edu>
2013-05-08 22:04:54 +0000

    *** empty log message ***

---
+++ emen2/db/create.py
@@ -0,0 +1,33 @@
+# $Id$
+"""Create a new database."""
+
+import os
+import sys
+import time
+import tarfile
+import tempfile
+import string
+import random
+import collections
+import getpass
+import json
+import jsonrpc.jsonutil
+
+# EMEN2 imports
+import emen2.util.listops
+import emen2.db.config
+
+
+
+class CreateOptions(emen2.db.config.DBOptions):
+    def parseArgs(self):
+        pass
+
+        
+if __name__ == "__main__":
+    import emen2.db
+    db = emen2.db.opendb(admin=True)
+    db._db.dbenv.create()
+    emen2.db.database.setup(db=db)
+            
+        
