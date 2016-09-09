788e6e7bdef53fa1c92fcb4fea7dfa20ea2409af
James Michael Bell <jmbell91@gmail.com>
2016-09-09 14:55:03 -0500

    replaced Site._escape with cgi.escape or html.escape

--- emen2/web/server.py
+++ emen2/web/server.py
@@ -8,6 +8,11 @@ import time
 import collections
 import sys
 
+try:
+    from cgi import escape
+except ImportError:
+    from html import escape
+
 # Twisted imports
 from twisted.application import internet
 import twisted.internet
@@ -88,13 +93,13 @@ class EMEN2Site(twisted.web.server.Site):
             ctxid,
             username,
             self._logDateTime,
-            '%s %s %s' % (self._escape(request.method),
-                          self._escape(request.uri),
-                          self._escape(request.clientproto)),
+            '%s %s %s' % (escape(request.method),
+                          escape(request.uri),
+                          escape(request.clientproto)),
             request.code,
             request.sentLength or "-",
-            self._escape(request.getHeader("referer") or "-"),
-            self._escape(request.getHeader("user-agent") or "-"))
+            escape(request.getHeader("referer") or "-"),
+            escape(request.getHeader("user-agent") or "-"))
 
         emen2.db.log.web(line)
 
@@ -133,7 +138,7 @@ class EMEN2BaseServer(object):
 
     def attach_resources(self, root):
         pass
-        
+
     def attach_to_service(self, service):
         emen2_service = internet.TCPServer(self.port, self.site)
         emen2_service.setServiceParent(service)
@@ -158,7 +163,7 @@ class EMEN2RPCServer(EMEN2BaseServer):
         import jsonrpc.server
         from emen2.web.resource import JSONRPCServerEvents
         root.putChild('jsonrpc', jsonrpc.server.JSON_RPC().customize(JSONRPCServerEvents))
-    
+
 class EMEN2WebServer(EMEN2BaseServer):
     """Start the full web server."""
     def attach_resources(self, root):
@@ -170,7 +175,7 @@ class EMEN2WebServer(EMEN2BaseServer):
         import jsonrpc.server
         import emen2.web.resource
 
-        # if all the JSON_RPC class does is change the eventhandler, 
+        # if all the JSON_RPC class does is change the eventhandler,
         #   it can (and should) be instantiated this way:
         from emen2.web.resource import JSONRPCServerEvents
         root.putChild('jsonrpc', jsonrpc.server.JSON_RPC().customize(JSONRPCServerEvents))
@@ -178,7 +183,7 @@ class EMEN2WebServer(EMEN2BaseServer):
         root.putChild('static-%s'%emen2.__version__, twisted.web.static.File(emen2.db.config.get_filename('emen2', 'web/static')))
         root.putChild('favicon.ico', twisted.web.static.File(emen2.db.config.get_filename('emen2', 'web/static/favicon.ico')))
         root.putChild('robots.txt', twisted.web.static.File(emen2.db.config.get_filename('emen2', 'web/static/robots.txt')))
-    
+
 def standalone(service='web'):
     opts = emen2.db.config.DBOptions()
     args = opts.parse_args()
@@ -191,6 +196,6 @@ def standalone(service='web'):
     server = service()
     emen2.db.log.info("Service started: %s"%service)
     server.start()
-    
+
 if __name__ == "__main__":
     standalone()
